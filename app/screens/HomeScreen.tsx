6e7ae9f6dadb30f67c64e3b92bd8bf56d8102197
maxrogers78 <maxrogerssepulveda@gmail.com>
2021-05-23 17:47:23 -0400

    interface de resultados de peticion añadida

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
+// interfaces
+import { MovieDBNowPlaying } from "../interfaces/movieInterface";
 // api
 import movieDB from "../api/movieDB";
 
 export const HomeScreen = () => {
   useEffect(() => {
-    movieDB.get("/now_playing").then((resp) => {
-      console.log(resp.data);
+    movieDB.get<MovieDBNowPlaying>("/now_playing").then((resp) => {
+      console.log(resp.data.results[0].title);
     });
   }, []);
 
