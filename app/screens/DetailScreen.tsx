7f743e69a8889f7bef3b51f4c739f1c1eed3b878
maxrogers78 <maxrogerssepulveda@gmail.com>
2021-05-24 10:37:26 -0400

    poster y titulos de movie en DetailScreen agregados

--- app/screens/DetailScreen.tsx
+++ app/screens/DetailScreen.tsx
@@ -1,20 +1,75 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Dimensions,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Text,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 // interfaces & types
 import { RootStackParams } from "../navigation/Navigation";
 
 interface Props extends StackScreenProps<RootStackParams, "DetailScreen"> {}
 
+const { height: screenHeight } = Dimensions.get("screen");
+
 export const DetailScreen = ({ route }: Props) => {
   const { top } = useSafeAreaInsets();
-  const { original_title } = route.params;
+
+  const movie = route.params;
+  const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
 
   return (
-    <View style={{ marginTop: top }}>
-      <Text>Detail Screen</Text>
-      <Text>{original_title}</Text>
-    </View>
+    <ScrollView>
+      <View style={{ ...styles.imageContainer, marginTop: top }}>
+        <Image
+          source={{
+            uri,
+          }}
+          style={styles.posterImage}
+        />
+      </View>
+
+      <View style={styles.marginContainer}>
+        <Text style={styles.subtitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: "100%",
+    height: screenHeight * 0.7,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+    elevation: 7,
+    overflow: "hidden",
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25,
+  },
+  posterImage: {
+    flex: 1,
+  },
+  marginContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  subtitle: {
+    fontSize: 16,
+    opacity: 0.4,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
